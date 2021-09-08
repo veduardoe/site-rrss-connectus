2f0261d95406ec9a4c349260875e85c22603d051
Eduardo Villalobos <veduardoe@gmail.com>
2021-09-08 18:35:52 -0300

    Más cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,13 @@
 export const ENV = {
-  prod: true,
-  locale: { es: 'es-CL', en: 'en-US'},
-  production : false,
-  BACKEND: 'https://srv-property-dot-globalproperty.uc.r.appspot.com',
-  FOTOS_PERFIL:'https://firebasestorage.googleapis.com/v0/b/globalpropertystorage-1d5b8.appspot.com/o/fotos-perfil%2F',
+  prod: false,
+  locale: { es: 'es-CL', en: 'en-US' },
+  production: false,
+  BACKEND: 'https://certain-pagoda-320920.uc.r.appspot.com',
+  FOTOS_PERFIL: 'https://firebasestorage.googleapis.com/v0/b/connectusstore.appspot.com/o/fotos-perfil%2F',
+  FICHEROS: 'https://firebasestorage.googleapis.com/v0/b/connectusstore.appspot.com/o/ficheros%2F',
+  MONTHS: {
+    es: ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC'],
+    en: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+  },
   sideMode: false
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,6 @@ export const ENV = {
   locale: { es: 'es-CL', en: 'en-US' },
   production: false,
   BACKEND: 'https://certain-pagoda-320920.uc.r.appspot.com',
-  //  BACKEND: 'https://srv-property-dot-globalproperty.uc.r.appspot.com',
   FOTOS_PERFIL: 'https://firebasestorage.googleapis.com/v0/b/connectusstore.appspot.com/o/fotos-perfil%2F',
   FICHEROS: 'https://firebasestorage.googleapis.com/v0/b/connectusstore.appspot.com/o/ficheros%2F',
   MONTHS: {
